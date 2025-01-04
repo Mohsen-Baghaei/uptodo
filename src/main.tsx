9919d56590d422b316763ac763dffff989c0a8c4
MOHSEN BAGHAEI <mohsen.bhi99@gmail.com>
2025-01-04 23:02:26 +0330

    done

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
